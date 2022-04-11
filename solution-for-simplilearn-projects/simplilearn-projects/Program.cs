cc65f7ed87aa171b6064b13d8e06f9e591238ff2
sai charan <charansai5274@gmail.com>
2022-04-11 11:31:03 +0530

    file-1 is added

---
+++ solution-for-simplilearn-projects/simplilearn-projects/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace simplilearn_projects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string file = @"C:\Users\chara\Documents\student details of rainbow school.txt";
+            string[] details = File.ReadAllLines(file);
+            foreach (string ln in details)
+                Console.WriteLine(ln);
+        }
+    }
+}
